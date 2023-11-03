5bad794e8c7002006814ce30b6ee5623b82dde25
Alex Frizziero <alex.frizziero@edu.unife.it>
2023-11-03 08:24:32 +0100

    riorganizzazione

--- connessione.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$link = mysqli_connect("127.0.0.1", "root", "password", "BIBLIOTECA");
-	if (!$link) {
-    		echo "Si รจ verificato un errore: impossibile collegarsi al database" . PHP_EOL;
-    		echo "Codice errore: " . mysqli_connect_errno() . PHP_EOL;
-    		echo "Messaggio errore: " . mysqli_connect_error() . PHP_EOL;
-    		exit(-1);
-	}
-?>
